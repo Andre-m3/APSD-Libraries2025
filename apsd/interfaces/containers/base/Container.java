ce43dbb1a6fa328fd66cfa481557b0ce8a8a10d3
Andre-m3 <andrew.mnt02@gmail.com>
2025-11-01 16:44:03 +0100

    container fix

--- apsd/interfaces/containers/base/Container.java
+++ apsd/interfaces/containers/base/Container.java
@@ -1,7 +1,5 @@
 package apsd.interfaces.containers.base;
 
-@SuppressWarnings("unused")   // Aggiunta temporanea per avere una progressione di sviluppo pulita
-
 /** Interface: Container, la base di tutti i contenitori. */
 public interface Container {
 
