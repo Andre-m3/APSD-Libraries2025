3eefe374884b72f91f3c243dc6f9b9b5c91043b8
Andre-m3 <andrew.mnt02@gmail.com>
2025-11-09 17:25:00 +0100

    deqs interfaces

--- apsd/interfaces/containers/deqs/Queue.java
+++ apsd/interfaces/containers/deqs/Queue.java
@@ -6,21 +6,34 @@ import apsd.interfaces.containers.base.InsertableContainer;
 public interface Queue<Data> extends ClearableContainer, InsertableContainer<Data> {
   
   // Head
+  abstract Data Head();
+
   // Dequeue
+  abstract void Dequeue();
+
   // HeadNDequeue
+  default Data HeadNDequeue() {
+    Data head = Head(); Dequeue();
+    return head;
+  }
 
   // Enqueue
+  abstract void Enqueue(final Data val);
 
   /* ************************************************************************ */
   /* Override specific member functions from ClearableContainer               */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default void Clear() { while (!IsEmpty()) { Dequeue(); } }
+  // We "Dequeue() the Head()" until the whole Queue is empty!
 
   /* ************************************************************************ */
   /* Override specific member functions from InsertableContainer              */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default boolean Insert(final Data val) { Enqueue(val); return true; }
+  // Enqueue(val) -> val is now our queue tail! (FIFO)
 
 }
--- apsd/interfaces/containers/deqs/Stack.java
+++ apsd/interfaces/containers/deqs/Stack.java
@@ -6,24 +6,45 @@ import apsd.interfaces.containers.base.InsertableContainer;
 public interface Stack<Data> extends ClearableContainer, InsertableContainer<Data> {
   
   // Top
+  abstract Data Top();
+
   // Pop
+  abstract void Pop();
+
   // TopNPop
+  default Data TopNPop() {
+    Data top = Top(); Pop();
+    return top;
+  } 
 
   // SwapTop
+  default void SwapTop(final Data val) {
+    Pop(); Push(val);
+  }
+
   // TopNSwap
+  default Data TopNSwap(final Data val) {
+    Data oldTop = TopNPop(); Push(val);
+    return oldTop;
+  }
 
   // Push
+  abstract void Push(final Data val);
 
   /* ************************************************************************ */
   /* Override specific member functions from ClearableContainer               */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default void Clear() { while (!IsEmpty()) { Pop(); } }
+  // We "Pop() the Top()" until the whole Stack is empty!
 
   /* ************************************************************************ */
   /* Override specific member functions from InsertableContainer              */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default boolean Insert(final Data val) { Push(val); return true; } 
+  // Push(val) -> val is now our new Stack head! (LIFO)
 
 }
