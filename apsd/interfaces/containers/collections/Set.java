1046e546f4701f8e54a64f3b95b6748cf3deafa9
Andre-m3 <andrew.mnt02@gmail.com>
2025-11-10 23:16:07 +0100

    interfaces

--- apsd/interfaces/containers/collections/Set.java
+++ apsd/interfaces/containers/collections/Set.java
@@ -1,20 +1,35 @@
 package apsd.interfaces.containers.collections;
 
-// import apsd.interfaces.containers.base.IterableContainer;
+import apsd.interfaces.containers.base.IterableContainer;
 
-public interface Set<Data> extends Collection<Data> { // Must extend Collection
-
+public interface Set<Data> extends Collection<Data> {
 
   // Union
+  default void Union(final Set<Data> set) {
+    set.TraverseForward(dat -> { Insert(dat); return false; });
+  }
 
   // Difference
+  default void Difference(final Set<Data> other) {
+    other.TraverseForward(dat -> { Remove(dat); return false; });
+  }
 
   // Intersection
+  default void Intersection(final Set<Data> set) { Filter(dat -> set.Exists(dat)); }
 
   /* ************************************************************************ */
   /* Override specific member functions from IterableContainer                */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  default boolean IsEqual(final IterableContainer<Data> other) {
+    if (other instanceof Set) {
+      if (!Size().equals(other.Size())) {
+        return false;
+      }
+      return TraverseForward(dat -> other.Exists(dat));
+    }
+    return false;
+  }
 
 }
