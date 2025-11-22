6906a8534e4e0b9ffd6dca4d7e104054dd5c0f9b
Andre-m3 <andrew.mnt02@gmail.com>
2025-11-22 18:11:51 +0100

    concreteCollections pt1

--- apsd/classes/containers/collections/concretecollections/VSortedChain.java
+++ apsd/classes/containers/collections/concretecollections/VSortedChain.java
@@ -1,34 +1,88 @@
 package apsd.classes.containers.collections.concretecollections;
 
-// import apsd.classes.containers.collections.concretecollections.bases.VChainBase;
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.TraversableContainer;
-// import apsd.interfaces.containers.collections.SortedChain;
-// import apsd.interfaces.containers.sequences.DynVector;
+import apsd.classes.containers.collections.concretecollections.bases.VChainBase;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.collections.SortedChain;
+import apsd.interfaces.containers.sequences.DynVector;
 
 /** Object: Concrete set implementation via (dynamic circular) vector. */
-public class VSortedChain<Data> { // Must extend VChainBase and implements SortedChain
+public class VSortedChain<Data extends Comparable<? super Data>> extends VChainBase<Data> implements SortedChain<Data> { // Must extend VChainBase and implements SortedChain
 
-  // public VSortedChain()
+  public VSortedChain() {
+    super();
+  }
 
-  // public VSortedChain(VSortedChain<Data> chn)
+  public VSortedChain(VSortedChain<Data> chn) {
+    super(chn);
+  }
 
-  // public VSortedChain(TraversableContainer<Data> con)
+  public VSortedChain(TraversableContainer<Data> con) {
+    con.TraverseForward(dat -> { Insert(dat); return false;});
+  }
 
-  // protected VSortedChain(DynVector<Data> vec)
+  protected VSortedChain(DynVector<Data> vec) {
+    super(vec);
+  }
 
-  // NewChain
+  @Override
+  protected VChainBase<Data> NewChain(DynVector<Data> vec) {
+    return new VSortedChain<>(vec);
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from InsertableContainer              */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public boolean Insert(Data dat) {
+    if (dat == null) { return false; }
+    Natural prd = SearchPredecessor(dat);
+    Natural pos = (prd == null) ? Natural.ZERO : prd.Increment();
+    /*vec.ShiftRight(pos); /// ERRORE TEST
+    vec.SetAt(dat, pos);*/
+    vec.InsertAt(dat, pos);
+    return true;
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from Chain                            */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public boolean InsertIfAbsent(Data val) {
+    // Sfruttiamo la ricerca binaria di SortedChain per efficienza
+    if (Search(val) != null) {
+      return false;
+    }
+    return Insert(val);
+  }
+
+@Override
+  public void RemoveOccurrences(Data val) {
+    // Implementazione Reader/Writer (due indici)
+    long size = vec.Size().ToLong();
+    long rdr = 0;
+    long wrt = 0;
+
+    while (rdr < size) {
+      Data curr = vec.GetAt(Natural.Of(rdr));
+      int cmp = curr.compareTo(val);
+      
+      if (cmp == 0) {
+          // Elemento da rimuovere
+      } else {
+          // Elemento da tenere
+          if (rdr != wrt)
+            vec.SetAt(curr, Natural.Of(wrt));
+          wrt++;
+        }
+      rdr++;
+    }
+
+    // Se qualcosa è stato rimosso, va modificata (ridotta) la size!
+    if (wrt < size)
+      vec.Reduce(Natural.Of(size - wrt));
+  }
 
 }
--- apsd/classes/containers/collections/concretecollections/bases/VChainBase.java
+++ apsd/classes/containers/collections/concretecollections/bases/VChainBase.java
@@ -1,65 +1,138 @@
 package apsd.classes.containers.collections.concretecollections.bases;
 
-// import apsd.classes.containers.sequences.DynCircularVector;
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.TraversableContainer;
-// import apsd.interfaces.containers.collections.Chain;
-// import apsd.interfaces.containers.iterators.BackwardIterator;
-// import apsd.interfaces.containers.iterators.ForwardIterator;
-// import apsd.interfaces.containers.iterators.MutableForwardIterator;
-// import apsd.interfaces.containers.sequences.DynVector;
-// import apsd.interfaces.containers.sequences.Sequence;
-// import apsd.interfaces.traits.Predicate;
+import apsd.classes.containers.sequences.DynCircularVector;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.collections.Chain;
+import apsd.interfaces.containers.iterators.BackwardIterator;
+import apsd.interfaces.containers.iterators.ForwardIterator;
+import apsd.interfaces.containers.iterators.MutableBackwardIterator;
+import apsd.interfaces.containers.iterators.MutableForwardIterator;
+import apsd.interfaces.containers.sequences.DynVector;
+import apsd.interfaces.containers.sequences.Sequence;
+import apsd.interfaces.traits.Predicate;
 
 /** Object: Abstract list base implementation on (dynamic circular) vector. */
-abstract public class VChainBase<Data> { // Must implement Chain
+abstract public class VChainBase<Data> implements Chain<Data> { // Must implement Chain
 
-  // protected final DynVector<Data> vec;
+  protected final DynVector<Data> vec;
 
-  // VChainBase
+  public VChainBase() {
+    vec = new DynCircularVector<>();
+  }
 
-  // NewChain
+  protected VChainBase(DynVector<Data> vec) {
+    this.vec = vec;
+  }
+
+  public VChainBase(TraversableContainer<Data> con) {
+    vec = new DynCircularVector<>(con);
+  }
+
+  abstract protected VChainBase<Data> NewChain(DynVector<Data> vec);
 
   /* ************************************************************************ */
   /* Override specific member functions from Container                        */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public Natural Size() { return vec.Size(); }
 
   /* ************************************************************************ */
   /* Override specific member functions from ClearableContainer               */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public void Clear() { vec.Clear(); }
 
   /* ************************************************************************ */
   /* Override specific member functions from RemovableContainer               */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public boolean Remove(Data dat) { 
+    Natural idx = vec.Search(dat);
+    if (idx != null) {
+      vec.RemoveAt(idx);
+      return true;
+    }
+    return false;
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from IterableContainer                */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public ForwardIterator<Data> FIterator() {
+    MutableForwardIterator<Data> iter = vec.FIterator();
+    return iter;
+  }
+
+  @Override
+  public BackwardIterator<Data> BIterator() {
+    MutableBackwardIterator<Data> iter = vec.BIterator();
+    return iter;
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from Sequence                         */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public Sequence<Data> SubSequence(Natural from, Natural to) {
+    // vec.SubVector restituisce un DynVector.
+    // Noi dobbiamo restituire una Sequence che sia coerente con questa classe (quindi una Chain).
+    // Usiamo il metodo astratto NewChain per "wrappare" il subvector in una nuova Chain.
+    return NewChain(vec.SubVector(from, to));
+  }
+
+  @Override
+  public Data GetAt(Natural index) {
+    return vec.GetAt(index);
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from RemovableAtSequence              */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public Data AtNRemove(Natural index) {
+    return vec.AtNRemove(index);
+  }
 
   /* ************************************************************************ */
   /* Override specific member functions from Collection                       */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public boolean Filter(Predicate<Data> pred) {
+    if (pred == null) return false;
 
+    // Implementazione ottimizzata O(N) per vettori (approccio rdr/wrt visto a lezione).
+    // Evito il richiamo di Remove(obj) che causerebbe n shift di elementi. (O(N^2))
+    long size = vec.Size().ToLong();
+    long wrt = 0; // Indice di scrittura
+
+    for (long rdr = 0; rdr < size; rdr++) {
+      Natural r = Natural.Of(rdr);
+      Data val = vec.GetAt(r);
+      
+      if (pred.Apply(val)) { // Se il predicato è vero, teniamo l'elemento
+        if (rdr != wrt) {
+          // Se siamo avanti col puntatore di lettura, spostiamo l'elemento indietro
+          vec.SetAt(val, Natural.Of(wrt));
+        }
+        wrt++;
+      }
+    }
+
+    // Se abbiamo scartato degli elementi (write < size), riduciamo la dimensione del vettore
+    if (wrt < size) {
+      vec.Reduce(Natural.Of(size - wrt));
+      return true;
+    }
+
+    return false;
+  }
 }
