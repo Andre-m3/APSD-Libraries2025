1f05b79343ee87ddc5c5d16c660f11d49d4a6234
Andre-m3 <andrew.mnt02@gmail.com>
2025-12-03 12:37:34 +0100

    wrapped classes

--- apsd/classes/containers/collections/abstractcollections/bases/WOrderedSetBase.java
+++ apsd/classes/containers/collections/abstractcollections/bases/WOrderedSetBase.java
@@ -1,25 +1,72 @@
 package apsd.classes.containers.collections.abstractcollections.bases;
 
-// import apsd.interfaces.containers.base.IterableContainer;
-// import apsd.interfaces.containers.base.TraversableContainer;
-// import apsd.interfaces.containers.collections.OrderedSet;
-// import apsd.interfaces.containers.collections.SortedChain;
+import apsd.interfaces.containers.base.IterableContainer;
+import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.collections.OrderedSet;
+import apsd.interfaces.containers.collections.SortedChain;
 
 /** Object: Abstract wrapper set base implementation via chain. */
-abstract public class WOrderedSetBase<Data, Chn> { // Must extend WSetBase and implement OrderedSet; Chn must extend SortedChain
+abstract public class WOrderedSetBase<Data extends Comparable<? super Data>, Chn extends SortedChain<Data>> extends WSetBase<Data, Chn> implements OrderedSet<Data> { // Must extend WSetBase and implement OrderedSet; Chn must extend SortedChain
 
   // WOrderedSetBase
+  public WOrderedSetBase() { super(); }
+
+  public WOrderedSetBase(Chn chn) { super(chn); }
+
+  public WOrderedSetBase(TraversableContainer<Data> con) { super(con); }
+
+  public WOrderedSetBase(Chn chn, TraversableContainer<Data> con) { super(chn, con); }
 
   /* ************************************************************************ */
   /* Override specific member functions from IterableContainer                */
   /* ************************************************************************ */
 
-  // ...
+  /**  In questa Base indico quale "super" usare per ciascun metodo.
+   * Altrimenti il richiamo sarebbe "ambiguo" e probabilmente errato!
+   * Non ci sono implementazioni ulteriori
+  */
+
+  @Override
+  public boolean IsEqual(IterableContainer<Data> con) { return chn.IsEqual(con); }
 
   /* ************************************************************************ */
   /* Override specific member functions from OrderedSet                       */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public Data Min() { return chn.Min(); }
+
+  @Override
+  public Data Max() { return chn.Max(); }
+  
+  @Override
+  public void RemoveMin() { chn.RemoveMin(); }
+
+  @Override
+  public void RemoveMax() { chn.RemoveMax(); }
+
+  @Override
+  public Data MinNRemove() { return chn.MinNRemove(); }
+
+  @Override
+  public Data MaxNRemove() { return chn.MaxNRemove(); }
+
+  @Override
+  public Data Predecessor(Data val) { return chn.Predecessor(val); }
+
+  @Override
+  public Data Successor(Data val) { return chn.Successor(val); }
+
+  @Override
+  public void RemovePredecessor(Data val) { chn.RemovePredecessor(val); }
+
+  @Override
+  public void RemoveSuccessor(Data val) { chn.RemoveSuccessor(val); }
+
+  @Override
+  public Data PredecessorNRemove(Data val) { return chn.PredecessorNRemove(val); }
+
+  @Override
+  public Data SuccessorNRemove(Data val) { return chn.SuccessorNRemove(val); }
 
 }
--- apsd/classes/containers/collections/abstractcollections/bases/WSetBase.java
+++ apsd/classes/containers/collections/abstractcollections/bases/WSetBase.java
@@ -1,62 +1,91 @@
 package apsd.classes.containers.collections.abstractcollections.bases;
 
-// import apsd.classes.utilities.Natural;
-// import apsd.interfaces.containers.base.TraversableContainer;
-// import apsd.interfaces.containers.collections.Chain;
-// import apsd.interfaces.containers.collections.Set;
-// import apsd.interfaces.containers.iterators.BackwardIterator;
-// import apsd.interfaces.containers.iterators.ForwardIterator;
-// import apsd.interfaces.traits.Predicate;
+import apsd.classes.utilities.Natural;
+import apsd.interfaces.containers.base.TraversableContainer;
+import apsd.interfaces.containers.collections.Chain;
+import apsd.interfaces.containers.collections.Set;
+import apsd.interfaces.containers.iterators.BackwardIterator;
+import apsd.interfaces.containers.iterators.ForwardIterator;
+import apsd.interfaces.traits.Predicate;
 
 /** Object: Abstract wrapper set base implementation via chain. */
-abstract public class WSetBase<Data, Chn> { // Must implement Set; Chn must extend Chain
+@SuppressWarnings("OverridableMethodCallInConstructor")
+abstract public class WSetBase<Data, Chn extends Chain<Data>> implements Set<Data> { // Must implement Set; Chn must extend Chain
 
   // protected Chn chn;
+  protected Chn chn;
 
   // WSetBase
+  public WSetBase() { ChainAlloc(); }
+
+  public WSetBase(Chn chn) { this.chn = chn; }
+
+  public WSetBase(TraversableContainer<Data> con) {
+    ChainAlloc();
+    con.TraverseForward(val -> { chn.InsertIfAbsent(val); return false; });
+    // Come spiegato a lezione, privilegiamo sempre la TraverseForward
+  }
+
+  public WSetBase(Chn chn, TraversableContainer<Data> con) {
+    this.chn = chn;
+    con.TraverseForward(val -> { chn.InsertIfAbsent(val); return false; });
+    // Come spiegato a lezione, privilegiamo sempre la TraverseForward
+  }
 
   // ChainAlloc
+  abstract protected void ChainAlloc();
 
   /* ************************************************************************ */
   /* Override specific member functions from Container                        */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public Natural Size() { return chn.Size(); }
 
   /* ************************************************************************ */
   /* Override specific member functions from ClearableContainer               */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public void Clear() { chn.Clear(); }
 
   /* ************************************************************************ */
   /* Override specific member functions from InsertableContainer              */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public boolean Insert(Data val) { return chn.InsertIfAbsent(val); }
 
   /* ************************************************************************ */
   /* Override specific member functions from RemovableContainer               */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public boolean Remove(Data val) { return chn.Remove(val); }
 
   /* ************************************************************************ */
   /* Override specific member functions from IterableContainer                */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public ForwardIterator<Data> FIterator() { return chn.FIterator(); }
+
+  @Override
+  public BackwardIterator<Data> BIterator() { return chn.BIterator(); }
 
   /* ************************************************************************ */
   /* Override specific member functions from Collection                       */
   /* ************************************************************************ */
 
-  // ...
+  @Override
+  public boolean Filter(Predicate<Data> pred) { return chn.Filter(pred); }
 
   /* ************************************************************************ */
   /* Override specific member functions from Set                              */
   /* ************************************************************************ */
 
-  // ...
+  // NOTA ATTENZIONE: In teoria non serve o sbaglio? Nel Diagrams è un errore?
+  // @Override
+  // public void Intersection(Set<Data> set) { chn.Intersection(set); }
 
 }
